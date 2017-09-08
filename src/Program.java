fccae7f80ca6b33d663522aa8edce4466f6ee2a8
Ivan Murzak <ivan.d.murzak@gmail.com>
2017-09-08 19:12:52 +0300

    Random thread sleep

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,24 @@
+import java.util.Random;
+
 public class Program {
-    public static void main(String[] args) {
-        
+    private final static Random r = new Random();
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Создаем поток");
+        new Thread(() -> {
+            while (true) {
+                int millisecondsToSleep = r.nextInt(4000) + 1000;
+                try {
+                    System.out.printf("%d поток засыпает на %d миллисекунд\n",
+                            Thread.currentThread().getId(),
+                            millisecondsToSleep);
+
+                    Thread.sleep(millisecondsToSleep);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        System.out.println("Запустили поток");
     }
 }
